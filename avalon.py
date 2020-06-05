ad32f38f9324af1022f9d2076d8d3d49f09058af
auspiciousArtifice <harrywang703@gmail.com>
2020-06-05 14:53:37 -0500

    Changes global game variable to be of a Session, adds getter and setter functions for Session

--- avalon.py
+++ avalon.py
@@ -12,20 +12,35 @@ class Vote(Enum):
     YEA = 1
 
 class Session:
-    def __init__(self):
-        self.players = {}
+    def __init__(self, host):
+        self.host = host
+        self.players = {host: None}
         self.state = GameState.CREATED
         self.quests_passed = 0
         self.quests_failed = 0
         self.current_quest = 0
         self.votes = {}
 
+    def get_host(self):
+        return self.host
+
+    def get_players(self):
+        return self.players
+
+    def add_player(self, player):
+        self.players[player] = None
+
+    def remove_player(self, player):
+        self.players.pop(player)
 
     def get_num_players(self):
         return len(self.players)
 
     def change_state(self, new_state):
         self.state = new_state
+
+    def get_state(self):
+        return self.state
 
     def cast_vote(self, player_name, vote_type):
         votes[player_name] = vote_type
